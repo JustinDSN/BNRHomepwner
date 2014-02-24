02696a43a2224c937a000534bad8da80cdeacafe
Justin Steffen <JustinDSN@gmail.com>
2014-02-24 12:54:30 -0800

    Adding BNRItemStore

---
+++ BNRItemStore.h
@@ -0,0 +1,20 @@
+//
+//  BNRItemsStore.h
+//  Homepwner
+//
+//  Created by Justin Steffen on 2/24/14.
+//  Copyright (c) 2014 Justin Steffen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class BNRItem;
+
+@interface BNRItemStore : NSObject
+
+@property (nonatomic, readonly) NSArray *allItems;
+
++ (instancetype)sharedStore;
+- (BNRItem *)createItem;
+
+@end
