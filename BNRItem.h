d9374381653f8aa4ca76bfb7bd4e3ed8cb8521e2
Justin Steffen <JustinDSN@gmail.com>
2014-02-24 12:38:52 -0800

    Adding BNRItem Adding BNRItemsViewController

---
+++ BNRItem.h
@@ -0,0 +1,28 @@
+//
+//  BNRItem.h
+//  RandomItems
+//
+//  Created by Justin Steffen on 2/22/14.
+//  Copyright (c) 2014 Justin Steffen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRItem : NSObject
+
+@property (nonatomic, copy) NSString *itemName;
+@property (nonatomic, copy) NSString *serialNumber;
+@property (nonatomic) int valueInDollars;
+@property (nonatomic, readonly, strong) NSDate *dateCreated;
+
++ (instancetype)randomItem;
+
+- (instancetype)initWithItemName:(NSString *)name
+                  valueInDollars:(int)value
+                    serialNumber:(NSString *)sNumber;
+
+- (instancetype)initWithItemName:(NSString *)name;
+
+- (instancetype)initWithItemName:(NSString *)name
+                    serialNumber:(NSString *)sNumber;
+@end
---
+++ BNRItemsViewController.m
@@ -0,0 +1,22 @@
+//
+//  BNRItemsViewController.m
+//  Homepwner
+//
+//  Created by Justin Steffen on 2/24/14.
+//  Copyright (c) 2014 Justin Steffen. All rights reserved.
+//
+
+#import "BNRItemsViewController.h"
+
+@implementation BNRItemsViewController
+
+- (instancetype)init {
+    self = [super initWithStyle:UITableViewStylePlain];
+    return self;
+}
+
+- (instancetype)initWithStyle:(UITableViewStyle)style {
+    return [self init];
+}
+
+@end
