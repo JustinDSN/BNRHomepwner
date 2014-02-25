f7894d00d1b85df3bcdba715d76701ddd3fa3ea8
Justin Steffen <JustinDSN@gmail.com>
2014-02-25 09:52:50 -0800

    Adding BNRDetailViewController

---
+++ Homepwner/BNRDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRDetailViewController.h
+//  Homepwner
+//
+//  Created by Justin Steffen on 2/25/14.
+//  Copyright (c) 2014 Justin Steffen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRDetailViewController : UIViewController
+
+@end
---
+++ Homepwner/BNRDetailViewController.m
@@ -0,0 +1,17 @@
+//
+//  BNRDetailViewController.m
+//  Homepwner
+//
+//  Created by Justin Steffen on 2/25/14.
+//  Copyright (c) 2014 Justin Steffen. All rights reserved.
+//
+
+#import "BNRDetailViewController.h"
+
+@interface BNRDetailViewController ()
+
+@end
+
+@implementation BNRDetailViewController
+
+@end
