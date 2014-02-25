2876b106c708cde01b1cb5c4dad969f820594f09
Justin Steffen <JustinDSN@gmail.com>
2014-02-25 10:56:41 -0800

    Adding Image view Adding Outlets and Actions

--- Homepwner/BNRDetailViewController.m
+++ Homepwner/BNRDetailViewController.m
@@ -15,6 +15,8 @@
 @property (weak, nonatomic) IBOutlet UITextField *serialNumberField;
 @property (weak, nonatomic) IBOutlet UITextField *valueField;
 @property (weak, nonatomic) IBOutlet UILabel *dateLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UIToolbar *toolbar;
 
 @end
 
@@ -55,4 +57,6 @@
     self.navigationItem.title = _item.itemName;
 }
 
+- (IBAction)takePicture:(id)sender {
+}
 @end
